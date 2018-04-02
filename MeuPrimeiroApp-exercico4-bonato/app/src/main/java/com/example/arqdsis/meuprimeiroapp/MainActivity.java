d349becce99f10855a798528caaca2a6555f0c6e
arqdsis <arqdsis@mocli10214.animaedu.intranet>
2018-04-02 20:38:45 -0300

    Initial commit

---
+++ MeuPrimeiroApp-exercico4-bonato/app/src/main/java/com/example/arqdsis/meuprimeiroapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.arqdsis.meuprimeiroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// create by Thamires
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
